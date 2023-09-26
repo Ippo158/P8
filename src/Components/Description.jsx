472fa9dda71ae2d0fb25fc2daa1521bf772b6c82
Ippo158 <bachy158@gmail.com>
2023-09-26 16:15:35 +0200

    description => a propos - DONE

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -1,15 +1,12 @@
 import "../CSS/Description.css";
 
-function Description({ show }) {
+function Description({ show, children }) {
   return (
     <div className={`description ${show ? "visible" : ""}`}>
-      <p className="description__text">
-        Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-        photos sont conformes aux logements, et toutes les informations sont
-        régulièrement vérifiées par nos équipes.
-      </p>
+      <p className="description__text">{children}</p>
     </div>
   );
 }
+
 
 export default Description;
--- src/Pages/A_propos.jsx
+++ src/Pages/A_propos.jsx
@@ -3,11 +3,22 @@ import Barre from "../Components/Barre";
 import "../CSS/About.css";
 import aboutImageUrl from "../Images/banniereimg2.png";
 import Description from "../Components/Description.jsx";
-
 import React, { useState } from "react";
 
 function A_propos() {
-  const [showDescription, setShowDescription] = useState(false);
+  const [showDescription, setShowDescription] = useState({
+    Fiabilité: false,
+    Respect: false,
+    Service: false,
+    Sécurité: false,
+  });
+
+  const toggleDescription = (title) => {
+    setShowDescription((prevState) => ({
+      ...prevState,
+      [title]: !prevState[title],
+    }));
+  };
 
   return (
     <div>
@@ -15,15 +26,37 @@ function A_propos() {
       <div className="container">
         <Barre
           title="Fiabilité"
-          onClick={() => setShowDescription(!showDescription)}
+          onClick={() => toggleDescription("Fiabilité")}
         />
-        <Description show={showDescription} />
+        <Description show={showDescription.Fiabilité}>
+          Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.
+        </Description>
 
-        <Barre title="Respect" />
+        <Barre title="Respect" onClick={() => toggleDescription("Respect")} />
+        <Description show={showDescription.Respect}>
+          La bienveillance fait partie des valeurs fondatrice de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </Description>
 
-        <Barre title="Service" />
+        <Barre title="Service" onClick={() => toggleDescription("Service")} />
+        <Description show={showDescription.Service}>
+          La bienveillance fait partie des valeurs fondatrice de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </Description>
 
-        <Barre title="Sécurité" />
+        <Barre title="Sécurité" onClick={() => toggleDescription("Sécurité")} />
+        <Description show={showDescription.Sécurité}>
+          La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les
+          standards sont bien respectés. Nous organiserons également des
+          ateliers sur la sécurité domestique pour nos hôtes.
+        </Description>
       </div>
     </div>
   );
